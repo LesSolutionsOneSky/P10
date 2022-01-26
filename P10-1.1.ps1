9f121851c7b5fd7b0eae7b1bae5fac2cc3f81712
mcajay01 <82970644+mcajay01@users.noreply.github.com>
2022-01-26 13:38:04 -0500

    Update P10-1.1.ps1

--- P10-1.1.ps1
+++ P10-1.1.ps1
@@ -5,12 +5,12 @@ fldclr
 
 Write-Host -ForegroundColor White "====== One Sky P10 ======"
 Write-Host -ForegroundColor Cyan "A: All - (Not Working)"
-Write-Host -ForegroundColor Yellow "1: Create OneSkyAdmin"
+Write-Host -ForegroundColor Yellow "1: Default Account/PC Name"
 Write-Host -ForegroundColor Yellow "2: Disable Administrators" 
 Write-Host -ForegroundColor Yellow "3: Install Applications"
 Write-Host -ForegroundColor Yellow "4: Enable Desktop Icons"
 Write-Host -ForegroundColor Yellow "5: Enable Hidden Items"
-Write-Host -ForegroundColor Yellow "6: Set Edge Home Page (Disabled)"
+Write-Host -ForegroundColor Yellow "6: Remove Bloat (Inactive)"
 Write-Host -ForegroundColor Yellow "7: Power Button (Shutdown)"
 Write-Host -ForegroundColor Yellow "8: Show All Tray icons"
 Write-Host -ForegroundColor Yellow "9: System Protection"
@@ -60,8 +60,30 @@ Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\C
 #Disable First Welcome Screen
 Write-Host "Disabled Windows Welcome Animation"
 Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name EnableFirstLogonAnimation -value 0
+
+#remove Taskbar Icons
+PinApp "Microsoft Store" -unpin
+PinApp "Mail" -unpin
+
 return DisMenu
 }
+function PinApp {    param(
+    [string]$appname,
+    [switch]$unpin
+)
+try{
+    if ($unpin.IsPresent){
+        ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | Where-Object{$_.Name -eq $appname}).Verbs() | Where-Object{$_.Name.replace('&','') -match 'Von "Start" lösen|Unpin from Start'} | Foreach-Object{$_.DoIt()}
+        return "App '$appname' unpinned from Start"
+    }else{
+        ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | Where-Object{$_.Name -eq $appname}).Verbs() | Where-Object{$_.Name.replace('&','') -match 'An "Start" anheften|Pin to Start'} | ForEach-object{$_.DoIt()}
+        return "App '$appname' pinned to Start"
+    }
+}catch{
+    Write-Error "Error Pinning/Unpinning App! (App-Name correct?)"
+}
+}
+
 
 #Function to Clear Temp Folder
 function fldclr {
@@ -77,13 +99,15 @@ function fldclr {
 }
 }
 #Create OneSky Account Function
-function AccCreate{
-  
-
-    
+function AccCreate
+{
     $password = Read-Host "Enter Defined Password:"
     cmd.exe /c net user OneSkyAdmin $password /add /comment:"Local Admin Account - One Sky"  
     Write-Host "Account Created!"
+
+    $Computername = Read-host "Enter New Computer Name: "
+    Rename-Computer -NewName $computername
+    Write-Host -ForegroundColor Red "Please Reboot after Configuration!"
     Start-Sleep 5
 
     Return DisMenu
@@ -112,47 +136,98 @@ function AdmDis
 #Application Installation
 function InstallApps
 {
-    #Clear-Host
-    Write-Host -ForegroundColor White "A: Install All Apps!"
+    Clear-Host
     Write-Host -ForegroundColor Green "1: Java"
     Write-Host -ForegroundColor Green "2: Adobe Reader DC" 
     Write-Host -ForegroundColor Green "3: Chrome"
     Write-Host -ForegroundColor Green "4: 7-Zip"
     Write-Host -ForegroundColor Green "5: ScreenConnect" 
     Write-Host -ForegroundColor Green "6: Automate"
+    Write-Host -ForegroundColor Green "7: Teams"
+    Write-Host -ForegroundColor Green "8: VLC"
     Write-Host -ForegroundColor Red "B: Press 'B' to Quit" 
 
     $SelectionApp = Read-Host "Please make a selection"
 
     Switch ($SelectionApp)
     {
-        'A' { 
-            Install-JRE
-            Install-Adobe
-            Install-Chrome
-            Install-Zip
-            Install-SC
-            Write-Host "All Apps Installed!"
-            Start-Sleep 5
-            Return DisMenu
-        }
         '1' { Install-JRE }
         '2' { Install-Adobe}
         '3' { Install-Chrome}
         '4' { Install-Zip}
         '5' { Install-SC }
         '6' { Install-AU }
+        '7' { Install-Teams }
+        '8' { Install-VLC }
         'B' { Return DisMenu }
     }
 }
 #Powerbutton Shutdown
 function Powerbtn
 {
-    cmd.exe /c "powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 0"
+    
     Write-Host "Power Button Set to Shutdown!"
+    cmd.exe /c "powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 0"
+
+    
+    Write-Host "Standby - Disabled"
+    cmd.exe /c "powercfg /change standby-timeout-AC 0 && powercfg /change standby-timeout-dc 0"
+
+    Write-Host "Disable Laptop Lid - <Set to Do Nothing>"
+    powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+    powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+    powercfg -SetActive SCHEME_CURRENT
     Start-Sleep 5
     Return DisMenu
 }
+#Teams installation
+function Install-Teams
+{
+    Write-Progress -Activity 'Installation of Teams' -Status 'Downloading' -PercentComplete 0
+        $DownloadPath = "https://go.microsoft.com/fwlink/p/?LinkID=869426&clcid=0x409&culture=en-us&country=US&lm=deeplink&lmsrc=groupChatMarketingPageWeb&cmpid=directDownloadWin64"
+        $SoftwarePath = "C:\Temp\Teams_Windows_x64.exe"
+    Write-Progress -Activity 'Installation of Teams' -Status 'Installing' -PercentComplete 25
+        $WebClient = New-Object System.Net.WebClient
+        $WebClient.DownloadFile($DownloadPath, $SoftwarePath)
+        $InstallExitCode = (Start-Process -FilePath $SoftwarePath -ArgumentList "-s" -Wait -Verb RunAs -PassThru)
+        Write-Progress -Activity 'Installation of Teams' -Status 'Cleaning Up' -PercentComplete 87
+        Start-Sleep 15
+        If ($InstallExitCode -eq 0) {
+            If (!$Silent) {Write-Progress -Activity 'Installation of Teams' -Status 'Completed' -PercentComplete 90}
+            New-NetQosPolicy -Name "Teams Audio" -AppPathNameMatchCondition "Teams.exe" -IPProtocolMatchCondition Both -IPSrcPortStartMatchCondition 50000 -IPSrcPortEndMatchCondition 50019 -DSCPAction 46 -NetworkProfile All
+            New-NetQosPolicy -Name "Teams Video" -AppPathNameMatchCondition "Teams.exe" -IPProtocolMatchCondition Both -IPSrcPortStartMatchCondition 50020 -IPSrcPortEndMatchCondition 50039 -DSCPAction 34 -NetworkProfile All
+            New-NetQosPolicy -Name "Teams Sharing" -AppPathNameMatchCondition "Teams.exe" -IPProtocolMatchCondition Both -IPSrcPortStartMatchCondition 50040 -IPSrcPortEndMatchCondition 50059 -DSCPAction 18 -NetworkProfile All
+            Write-Progress -Activity 'Installation of Teams' -Status 'QOS Enabled' -PercentComplete 100
+            Return InstallApps
+        } Else {
+            Write-Progress -Activity 'Installation of Teams' -Status 'Failed'
+            Return InstallApps
+        }# End Else
+        Return InstallApps
+}
+
+#VLC Installation
+function Install-VLC
+{
+    Write-Progress -Activity 'Installation of VLC' -Status 'Downloading' -PercentComplete 0
+        $DownloadPath = "https://vlc.freemirror.org/vlc/3.0.16/win64/vlc-3.0.16-win64.exe"
+        $SoftwarePath = "C:\Temp\vlc-3.0.16-win64.exe"
+    Write-Progress -Activity 'Installation of VLC' -Status 'Installing' -PercentComplete 25
+        $WebClient = New-Object System.Net.WebClient
+        $WebClient.DownloadFile($DownloadPath, $SoftwarePath)
+        $InstallExitCode = (Start-Process -FilePath $SoftwarePath -ArgumentList "/L=1033 /S" -Wait -Verb RunAs -PassThru)
+        Write-Progress -Activity 'Installation of VLC' -Status 'Cleaning Up' -PercentComplete 87
+        Start-Sleep 15
+        If ($InstallExitCode -eq 0) {
+            If (!$Silent) {Write-Progress -Activity 'Installation of VLC' -Status 'Completed' -PercentComplete 100}
+            Return InstallApps
+        } Else {
+            Write-Progress -Activity 'Installation of VLC' -Status 'Failed'
+            Return InstallApps
+        }# End Else
+        Return InstallApps
+}
+
 #Java Installation
 function Install-JRE
 {
@@ -167,12 +242,12 @@ function Install-JRE
         Start-Sleep 15
         If ($InstallExitCode -eq 0) {
             If (!$Silent) {Write-Progress -Activity 'Installation of Java' -Status 'Completed' -PercentComplete 100}
-            Return DisMenu
+            Return InstallApps
         } Else {
             Write-Progress -Activity 'Installation of Java' -Status 'Failed'
-            Return DisMenu
+            Return InstallApps
         }# End Else
-        Return DisMenu
+        Return InstallApps
 }
 #Installation Chrome
 function Install-Chrome
@@ -188,26 +263,26 @@ function Install-Chrome
         Start-Sleep 15
         If ($InstallExitCode -eq 0) {
             If (!$Silent) {Write-Progress -Activity 'Installation of Chrome' -Status 'Completed' -PercentComplete 100}
-            Return DisMenu
+            Return InstallApps
         } Else {
             Write-Progress -Activity 'Installation of Chrome' -Status 'Failed'
-            Return DisMenu
+            Return InstallApps
         }# End Else
-        Return DisMenu
+        Return InstallApps
 }
 #Installation Adobe Reader DC
 function Install-Adobe
 {
     Write-Progress -Activity 'Installation of Adobe' -Status 'Downloading' -PercentComplete 0
         $DownloadPath = "https://get.adobe.com/reader/completion/?installer=Reader_DC_2021.007.20099_French_for_Windows&stype=7787&direct=true&standalone=1"
-        $SoftwarePath = "Reader_DC_2021.007.20099_French_for_Windows.exe"
+        $SoftwarePath = "C:\Temp\Reader_DC_2021.007.20099_French_for_Windows.exe"
     Write-Progress -Activity 'Installation of Adobe' -Status 'Installing' -PercentComplete 25
         $WebClient = New-Object System.Net.WebClient
         $WebClient.DownloadFile($DownloadPath, $SoftwarePath)
         Start-Process -FilePath $SoftwarePath -ArgumentList "-sfx_nu /sAll /rs /msi EULA_ACCEPT=YES" -Wait -Verb RunAs -PassThru
         Write-Progress -Activity 'Installation of Adobe' -Status 'Cleaning Up' -PercentComplete 87
         
-        Return DisMenu
+        Return InstallApps
 }
 #Installation 7-Zip
 function Install-Zip
@@ -223,12 +298,12 @@ function Install-Zip
         Start-Sleep 15
         If ($InstallExitCode -eq 0) {
             If (!$Silent) {Write-Progress -Activity 'Installation of Zip' -Status 'Completed' -PercentComplete 100}
-            Return DisMenu
+            Return InstallApps
         } Else {
             Write-Progress -Activity 'Installation of Zip' -Status 'Failed'
-            Return DisMenu
+            Return InstallApps
         }# End Else
-        Return DisMenu
+        Return InstallApps
 }
 #INstallation ScreenConnect
 function Install-SC
@@ -244,12 +319,12 @@ function Install-SC
         Start-Sleep 15
         If ($InstallExitCode -eq 0) {
             If (!$Silent) {Write-Progress -Activity 'Installation of ScreenConnect' -Status 'Completed' -PercentComplete 100}
-            Return DisMenu
+            Return InstallApps
         } Else {
             Write-Progress -Activity 'Installation of ScreenConnect' -Status 'Failed'
-            Return DisMenu
+            Return InstallApps
         }# End Else
-        Return DisMenu
+        Return InstallApps
 }
 function Install-AU
 {
@@ -257,6 +332,7 @@ function Install-AU
 Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/LesSolutionsOneSky/Automate/main/Deploy.ps1')
 
 }
+
 #Enable Desktop Icons
 function DskIcons
 {
@@ -272,6 +348,10 @@ function DskIcons
     #Enable This PC Icon
     Set-Itemproperty -path 'Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel' -Name '{20D04FE0-3AEA-1069-A2D8-08002B30309D}' -value '0' -Force
     New-Itemproperty -path 'Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu' -Name '{20D04FE0-3AEA-1069-A2D8-08002B30309D}' -value '0' -Type Dword -Force
+
+    #Enable Control Panel Icon
+    Set-Itemproperty -path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel' -Name '{5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0}' -value '0' -Force
+    New-Itemproperty -path 'Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu' -Name '{5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0}' -value '0' -Type Dword -Force
 
     Stop-Process -ProcessName Explorer
     Start-Sleep 5
@@ -308,10 +388,6 @@ function DisBluescreen
     Write-Host "Shadow Copy Enabled Drive C:\"
     Start-Process "%windir%\System32\SystemPropertiesProtection.exe"
 
-    Write-Host "Disable Laptop Lid - <Set to Do Nothing>"
-    powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
-    powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
-    powercfg -SetActive SCHEME_CURRENT
     Start-Sleep 10
     Write-Host -ForegroundColor Green "Done!"
 
@@ -350,5 +426,6 @@ If($status -Like 'False')
     Start-Sleep 5
 Break
 } else {#>
+    Set-WinUserLanguageList -LanguageList en-US, fr-CA -Force
 DisMenu
 #}
